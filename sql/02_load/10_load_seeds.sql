8a150825b294e45c18c4db1582d04d2ff18b7399
Ishaan <Ishaan.Madhivanan@gmail.com>
2026-08-25 13:09:39 -0400

    Fix seed load: stage county CSV before insert

--- sql/02_load/10_load_seeds.sql
+++ sql/02_load/10_load_seeds.sql
@@ -10,13 +10,24 @@
 USE ErcotSiting;
 GO
 
-DECLARE @repo NVARCHAR(400) = N'C:\projects\ercot-siting-screen';   -- <<< EDIT ME
+DECLARE @repo NVARCHAR(400) = N'D:\sql\ercot-siting-screen';   -- <<< EDIT ME
 
-TRUNCATE TABLE dim.county;
-EXEC('BULK INSERT dim.county
+DROP TABLE IF EXISTS stg.county_seed;
+CREATE TABLE stg.county_seed (
+    county_fips CHAR(5),
+    state_fips  CHAR(2),
+    county_name NVARCHAR(80),
+    match_key   VARCHAR(60)
+);
+
+EXEC('BULK INSERT stg.county_seed
       FROM ''' + @repo + '\data\seed\dim_county_tx.csv''
       WITH (FIRSTROW = 2, FIELDTERMINATOR = '','', ROWTERMINATOR = ''0x0a'', TABLOCK);');
 
+DELETE FROM dim.county;
+INSERT INTO dim.county (county_fips, state_fips, county_name, match_key)
+SELECT county_fips, state_fips, county_name, match_key
+FROM stg.county_seed;
 TRUNCATE TABLE dim.fuel;
 EXEC('BULK INSERT dim.fuel
       FROM ''' + @repo + '\data\seed\dim_fuel.csv''
